07d447baea785514cd48aa957fd8c877affeeaee
camila15 <mcamilas1599@gmail.com>
2022-05-17 10:48:43 -0500

    información de los participantes

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -3,31 +3,111 @@ import p40 from '../img/poli-desde-el-cielo.jpg'
 
 const places = [
     {
-        name: "Politécnico Jaime Isaza Cadavid",
-        description: "Entrada del Poli",
-        date: "Lunes 16/05/2022 Hora: 14:00",
+        name: "Estudio Alejandro Tobón",
+        description: "Instagram: @alejandrotob",
+        description2:"El segundo piso también estará abierto, este es un espacio de exhibición donde tenemos una gran variedad de artistas consagrados y emergentes. Allí también estará la artista Lorena Zuluaga con sus obras y posiblemente esté trabajando. Entrada gratuita y puede asistir público de todas las edades..",
+        date: "Sábado 04/06/2022 Hora: 12:00-6:00pm - Nivel 2 Fachada azul, puerta roja",
         image: porteria,
         geometry: [
-            6.212782164089769, 
-            -75.5770904927681
+            6.226171068814857, 
+            -75.57429908987079
         ]
     },
     {
-        name: "Bloque P-40",
-        description: "Bloque P-40",
-        date: "Lunes 16/05/2022 Hora: 14:00",
+        name: "El Coleccionista Galería",
+        description: "Actividad: Open estudio",
+        date: "Sábado 04/06/2022 Nivel 2 Fachada azul, puerta roja",
         image: p40,
         geometry: [
-            6.21053843842637, 
-            -75.57700846388329
+            6.226171068814857, 
+            -75.57429908987079
         ]
     },{
-        name: "Parque del Poblado",
-        description: "El parque del poblado",
-        date: "Lunes 16/05/2022 Hora: 14:00",
+        name: "Estudio Lorena Zuluaga",
+        description: "Actividad: Open estudio",
+        date: "Sábado 04/06/2022 Nivel 2 Fachada azul, puerta roja",
         geometry: [
-            6.21051863982928, 
-            -75.57098926150462
+            6.226171068814857, 
+            -75.57429908987079
+        ]
+    },{
+        name: "Atelier.4 ",
+        description: "Open estudio.Karte Uberall Vol.3, montaje enfocado en la pintura expandida hacia la multimedia, el autor es Douglas Gaviria quien estará in situ dialogando con el público en circulación, así mismo el resto de los socios departiendo sobre los diferentes frentes que se atienden en el taller.",
+        description2: "los integrantes son German Benjumea-Douglas Gaviria-Jhon Jairo Muriel-Jorge Mazuera",
+        date: "Sábado 04/06/2022 Nivel 2 Tercer piso",
+        geometry: [
+            6.227570302602989, 
+            -75.57377014569082
+        ]
+    },{
+        name: "LA HOJA TALLER DE ARTISTAS",
+        description: "Talleres de hojilla dorada. , Reservas a través de la cuenta de Instagram de La Hoja @lahoja_taller o al número 3164984697. Costo del taller: $60.000 por persona. Además de los talleres, el espacio La Hoja estará abierto al público de 9 am a 7 pm, para que los visitantes puedan conocer las obras de la exposición.",
+        date: "Sábado 04/06/2022 Hora: 10:00 am, 2:00 pm y 5:00 pm-piso 3 of. 301",
+        geometry: [
+            6.227075468373374, 
+            -75.57253820336284
+        ]
+    },{
+        name: "Estudio Daniel Estrada",
+        description: "Actividad: Open estudio",
+        date: "Sábado 04/06/2022 Nivel 2 ",
+        geometry: [
+            6.2276445991507705, 
+            -75.57378000336283
+        ]
+    },{
+        name: "Búnker Taller de Artistas",
+        description: "Actividad: Open estudio-Artistas: Andrés Layos, Carlos Carmona, Camilo Correa y Diego Díaz ",
+        date: "Sábado 04/06/2022 Nivel 2 y 3 ",
+        geometry: [
+            6.226594130805044, 
+            -75.57456147452677
+        ]
+    },{
+        name: "Danilo Cuadros",
+        description: "Actividad:Taller abierto",
+        date: "Sábado 04/06/2022",
+        geometry: [
+            6.226088209947457, 
+            -75.57424354754284
+        ]
+    },{
+        name: "Estudio Diego Diaz",
+        description: "Actividad:Taller abierto",
+        date: "Sábado 04/06/2022, piso 2",
+        geometry: [
+            6.2264874753588675, 
+            -75.57455074569077
+        ]
+    },{
+        name: "Madrigal Gourmet",
+        description: "Actividad:Ese día abriremos hasta las 8 am - 9 pm Especialidad parrilla y Burgers",
+        date: "Sábado 04/06/2022",
+        geometry: [
+            6.227004530571346, 
+            -75.5738385763788
+        ]
+    },{
+        name: "Cervecería Libre",
+        date: "Sábado 04/06/2022-Barrio Colombia, Abierto al público desde el mediodía.",
+        geometry: [
+            6.226973037341719, 
+            -75.57332254754282
+        ]
+    },{
+        name: "Feria Buen Provecho",
+        description: "Gastronomía local con antojos, platos fuertes, postres, cerveza artesanal y mucho más! @feriabuenprovecho ",
+        date: "Sábado 04/06/2022  12:00-22:00 entrada gratuita, Plazoleta MAMM",
+        geometry: [
+            6.224889589680118, 
+            -75.5742169359278
+        ]
+    },{
+        name: "Telemedellín",
+        date: "Sábado 04/06/2022",
+        geometry: [
+            6.21964303476057, 
+            -75.5705736321988
         ]
     }
 ];
